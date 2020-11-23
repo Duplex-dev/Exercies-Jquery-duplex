1885268cc16e1ac1ac863b1110580edb45177627
Duplex-dev <72447371+Duplex-dev@users.noreply.github.com>
2020-11-23 22:15:20 +0100

    Add files via upload

---
+++ exercice1/JS/jquery.js
@@ -0,0 +1,9 @@
+$('button').click(function(){
+    $('#form').css({
+        border: "2px solid red"
+    });
+    //$('p').append(borders)
+    $('p').css({
+        border: "2px solid red"
+    })
+})
